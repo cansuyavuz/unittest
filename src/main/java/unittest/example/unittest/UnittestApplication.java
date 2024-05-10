8103f96b1fdb608a5568577854fdf854694cfcb2
cansuyavuz <yavuzcan@students.zhaw.ch>
2024-05-10 22:42:47 +0200

    add

---
+++ src/main/java/unittest/example/unittest/UnittestApplication.java
@@ -0,0 +1,13 @@
+package unittest.example.unittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnittestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnittestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unittest/example/unittest/UnittestApplicationTests.java
@@ -0,0 +1,13 @@
+package unittest.example.unittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnittestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
