cf63b9fadbe3fc407b42988240af571163efa886
cansuyavuz <yavuzcan@students.zhaw.ch>
2024-05-10 23:11:39 +0200

    testpage erstellt

---
+++ src/test/java/unittest/example/unittest/ScoreKeeperTest.java
@@ -0,0 +1,9 @@
+package unittest.example.unittest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ScoreKeeperTest {
+    private ScoreKeeper scoreKeeper;
+}
